d437c83e85bcf1107a5d89be29dc6c9cacaf1dba
Sanjana42796 <sanjanasanjana42796@gmail.com>
2024-08-24 12:48:03 +0530

    Add files via upload

---
+++ javaa.js
@@ -0,0 +1,28 @@
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+
+var randomImageSource1 = "images/dice" + randomNumber1 + ".png";
+var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
+
+document.querySelectorAll("img")[0].setAttribute("src", randomImageSource1);
+document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
+
+
+    if (randomNumber1===5){
+        document.querySelector("h1").innerHTML = "Player 1 Wins! 🎉";
+    } 
+ else if(randomNumber2===6){
+        document.querySelector("h1").innerHTML = "Player 2 Wins !🏆 ";
+    }else{
+        document.querySelector("h1").innerHTML = "No one wins! 🤝";
+
+    }
+
+
+
+
+
+
+
+
